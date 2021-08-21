77175d1ac21caab58b926fba51842a00339e6f4d
simran.sidhu <simran.sidhu@servicenow.com>
2021-08-21 10:24:04 +0530

    User Profile

--- lib/pages/manage_account.dart
+++ lib/pages/manage_account.dart
@@ -57,28 +57,15 @@ class _ManageAccountState extends State<ManageAccount> {
                               alignment: Alignment.bottomRight,
                               children: <Widget>[
                                 CircleAvatar(
-                                  radius: 30.0,
+                                  radius: 40.0,
                                   backgroundImage: AssetImage(
-                                      "assets/images/fiverr_logo.png"),
-                                ),
-                                Container(
-                                  child: CircleAvatar(
-                                    radius: 6.0,
-                                    backgroundColor: Color(0xFF1DBF73),
-                                  ),
-                                  decoration: new BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: const Color(0xFFFFFFFF),
-                                    // border color
-                                    border: Border.all(
-                                        color: Colors.black87, width: 2),
-                                  ),
+                                      "assets/images/nouser.jpeg"),
                                 ),
                               ],
                             ),
                           ),
                           Text(
-                            "bruce",
+                            "Madan Mohan",
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 18.0,
@@ -88,7 +75,7 @@ class _ManageAccountState extends State<ManageAccount> {
                             height: 6.0,
                           ),
                           Text(
-                            "My personal balance: US\$36.90",
+                            "My personal balance: ₹3336.90",
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 14.0,
@@ -98,40 +85,7 @@ class _ManageAccountState extends State<ManageAccount> {
                       ),
                     ),
                   ),
-                  Container(
-                    height: 50.0,
-                    width: MediaQuery.of(context).size.width,
-                  ),
                 ],
-              ),
-              Card(
-                elevation: 8.0,
-                child: Container(
-                  height: 60.0,
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.credit_card,
-                      color: Theme.of(context).accentColor,
-                    ),
-                    title: Text(
-                      "Seller mode",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    trailing: Switch(
-                      value: isSeller,
-                      onChanged: (value) {
-                        setState(() {
-                          isSeller = value;
-                        });
-                      },
-                      activeTrackColor: Color(0xFF8bd9ad),
-                      activeColor: Theme.of(context).accentColor,
-                    ),
-                  ),
-                ),
               ),
             ],
           ),
@@ -142,7 +96,7 @@ class _ManageAccountState extends State<ManageAccount> {
                   padding:
                       const EdgeInsets.only(top: 20.0, left: 20.0, bottom: 5.0),
                   child: Text(
-                    "My Fiverr",
+                    "Yaar Help",
                     style: TextStyle(
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold,
@@ -159,7 +113,7 @@ class _ManageAccountState extends State<ManageAccount> {
                     );
                   },
                   child: ListTile(
-                    leading: Icon(Icons.monetization_on),
+                    leading: Text(' \u{20B9}', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 28, color: Colors.blue[500]),),
                     title: Text(
                       "Earnings",
                       style: TextStyle(
@@ -172,9 +126,9 @@ class _ManageAccountState extends State<ManageAccount> {
                 InkWell(
                   onTap: () {},
                   child: ListTile(
-                    leading: Icon(FontAwesomeIcons.solidStickyNote),
+                    leading: Icon(Icons.list_alt_rounded, size: 28, color: Colors.orange[700],),
                     title: Text(
-                      "Buyers requests",
+                      "My Orders",
                       style: TextStyle(
                         fontSize: 16.0,
                         fontWeight: FontWeight.bold,
@@ -185,9 +139,9 @@ class _ManageAccountState extends State<ManageAccount> {
                 InkWell(
                   onTap: () {},
                   child: ListTile(
-                    leading: Icon(FontAwesomeIcons.fileMedicalAlt),
+                    leading: Icon(Icons.check_circle, color: Colors.green, size: 28),
                     title: Text(
-                      "Offer templates",
+                      "Completed Orders",
                       style: TextStyle(
                         fontSize: 16.0,
                         fontWeight: FontWeight.bold,
@@ -198,22 +152,9 @@ class _ManageAccountState extends State<ManageAccount> {
                 InkWell(
                   onTap: () {},
                   child: ListTile(
-                    leading: Icon(FontAwesomeIcons.bullhorn),
+                    leading: Icon(Icons.pending_actions, size: 25, color: Colors.yellow[800],),
                     title: Text(
-                      "Promote my gigs",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    leading: Icon(FontAwesomeIcons.windowMaximize),
-                    title: Text(
-                      "My gigs",
+                      "Pending Orders",
                       style: TextStyle(
                         fontSize: 16.0,
                         fontWeight: FontWeight.bold,
