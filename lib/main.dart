32bb48dbc6da0e7c466edb66bf7143c8ba346a47
simran.sidhu <simran.sidhu@servicenow.com>
2021-08-31 12:05:33 +0530

    Get gigs from FB, add apply function. + update create gig

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fiverr_clone/pages/loginPage.dart';
+import 'package:fiverr_clone/pages/main_tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
